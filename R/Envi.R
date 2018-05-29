853fb34397189d08500cc12065385370edb4b652
jwons <joe.wonsil@gmail.com>
2018-05-29 10:46:10 -0400

    added support for sourced scripts

--- R/Envi.R
+++ R/Envi.R
@@ -2,12 +2,24 @@ eParse <- function(file) {
   library("jsonlite")
   
   prov.data <- fromJSON(file)
+
+  env <- prov.data$entity$environment
   
-  # environment
-  environment <- t(as.data.frame(prov.data$entity$environment))
+  # Source Scripts
+  scripts <-env$`rdt:sourcedScripts`
+  scripts <- as.data.frame(cbind(scripts, env$`rdt:sourcedScriptTimeStamps`))
+  names(scripts) <- c("Scripts", "Timestamps")
+  write.csv(scripts,"scripts.csv")
+
+  # Environment
+  env <- env[ - which(names(env) == "rdt:sourcedScripts")]
+  env <- env[ - which(names(env) == "rdt:sourcedScriptTimeStamps")]
+  
+  environment <- t(as.data.frame(env))
   colnames(environment) <- c("Value")
   write.csv(environment, "environment.csv")
   
+
   # libraries
   libraries <- prov.data$entity[grep("^l", names(prov.data$entity))]
   
