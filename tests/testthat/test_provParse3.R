1a58d665aa0d0e4c4426806a9718a67c64f53b31
Barbara Lerner <blerner@mtholyoke.edu>
2018-10-22 15:29:10 -0400

    Changed parser to create a ProvInfo object
    prov.parse now returns a ProvInfo object, which is then passed
    as a parameter to the access functions.  This allows a tool to
    be working with more than 1 parsed provenance at a time.
    
    Also, updated the documentation.

--- tests/testthat/test_provParse3.R
+++ tests/testthat/test_provParse3.R
@@ -3,14 +3,14 @@ library(testthat)
 
 ## Loading test data
 test.data <- system.file("testdata", "prov2.json", package = "provParseR")
-prov.parse(test.data)
+prov <- prov.parse(test.data)
 
 context("Scripts access function")
-scripts.df <- get.scripts()
+scripts.df <- get.scripts(prov)
 expect_match(class(scripts.df), "data.frame")
 expect_equal (nrow(scripts.df), 5)
 
-saved.scripts.df <- get.saved.scripts()
+saved.scripts.df <- get.saved.scripts(prov)
 expect_equal (nrow(saved.scripts.df), 5)
 directory <- dirname (saved.scripts.df$script[1])
 expect_equal (directory, 
