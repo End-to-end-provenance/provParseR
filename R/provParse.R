b5c6facd9c07654ce740a8cef3a2f9d23b432537
Barbara Lerner <blerner@mtholyoke.edu>
2018-12-20 13:05:58 -0500

    Corrected copyright.

--- R/provParse.R
+++ R/provParse.R
@@ -1,5 +1,5 @@
 # Copyright (C) President and Fellows of Harvard College and 
-# Trustees of Mount Holyoke College, 2014, 2015, 2016, 2017, 2018.
+# Trustees of Mount Holyoke College, 2018.
 
 # This program is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
