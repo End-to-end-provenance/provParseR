e3e4815e71dfd83bc3fdbde5734d85c3cc7f6944
fong22e <fong22e@mtholyoke.edu>
2019-06-18 12:09:04 -0400

    updated sourceScript test

--- tests/testthat/test_sourcedScripts.R
+++ tests/testthat/test_sourcedScripts.R
@@ -3,14 +3,30 @@ library(testthat)
 
 context("Reading list of sourced scripts")
 
-# cases
+# function to remove timestamp and file path from data frame comparison
+process.df <- function(df)
+{
+	# remove timestamp
+	df <- df[ , 1:2]
+	
+	# remove file paths from source script names
+	script <- df["script"]
+	sapply(script, basename)
+	
+	# replace script name column
+	df["script"] <- unname(script)
+	
+	return(df)
+}
+
+# cases - remove timestamp & file path from sourced script names
 c0 <- system.file("testdata/cases", "empty.json", package = "provParseR")
 c1 <- system.file("testdata/cases/test_sourcedScripts", "1SourcedScript.json", package = "provParseR")
 c4 <- system.file("testdata/cases/test_sourcedScripts", "4SourcedScripts.json", package = "provParseR")
 
-t0 <- get.scripts(prov.parse(c0))
-t1 <- get.scripts(prov.parse(c1))
-t4 <- get.scripts(prov.parse(c4))
+t0 <- process.df(get.scripts(prov.parse(c0)))
+t1 <- process.df(get.scripts(prov.parse(c1)))
+t4 <- process.df(get.scripts(prov.parse(c4)))
 
 # expected
 expected.file <- system.file("testdata/expected", "sourced-scripts.csv", package = "provParseR")
@@ -23,3 +39,4 @@ e1 <- e4[1:2, ]
 expect_equivalent(t0, e0)
 expect_equivalent(t1, e1)
 expect_equivalent(t4, e4)
+
