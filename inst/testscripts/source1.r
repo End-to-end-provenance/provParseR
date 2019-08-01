7f42d1bbf7945661ae1d8a65a8dfa626b01c1a8a
fong22e <fong22e@mtholyoke.edu>
2019-08-01 14:14:36 -0400

    edited sourcescript test

--- inst/testscripts/source1.r
+++ inst/testscripts/source1.r
@@ -1,2 +1,9 @@
+# this script assumes the existance of fun and z, and is used as a source
+# test case
+w <- z + x
+w <- fun(w,y)
+
+# WILL REMOVE THIS ONCE BARBARA'S CODE TO FIX COLLAPSIBLE NODE DATA NDOES IS FIXED
 z <- 0
+
 source("source2.r")
